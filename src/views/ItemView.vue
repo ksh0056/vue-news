dabeb7a115467a4364d3435c2dfa3c89b86d9068
Kim SeungHyuk <ksh9284@naver.com>
2019-11-13 00:16:40 +0900

    UserProfile 공통화

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
       <section>
-          <!-- 질문 상세 정보 -->
-          <div class="user-container">
-              <div>
-                  <i class="fas fa-user"></i>
-              </div>
-              <div class="user-description">
-                  <router-link :to="`/user/${fetchedItem.user}`" >
-                      {{ fetchedItem.user}}
-                  </router-link>
-                  <div class="time">
-                      {{ fetchedItem.time_ago }}
-                  </div>
-              </div>
-          </div>
+          <!-- 사용자 정보 -->
+          <user-profile :info="fetchedItem">
+              <!-- <div slot="username">{{ fetchedItem.user }}</div> -->
+              <router-link slot="username" :to="`/user/${fetchedItem.user}`">
+                {{ fetchedItem.user }}
+              </router-link>
+              <template slot="time">{{ 'Posted ' + fetchedItem.time_ago }}</template>
+          </user-profile>
+      </section>
+      <section>
           <h2>{{ fetchedItem.title }}</h2>
       </section>
       <section>
@@ -25,8 +21,13 @@
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue'
 import { mapGetters } from 'vuex'
+
 export default {
+    components: {
+        UserProfile
+    },
     computed: {
         ...mapGetters([
             'fetchedItem'
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import ListItem from '../components/LIstItem.vue'
+import ListItem from '../components/ListItem.vue'
 
 export default {
     components: {
