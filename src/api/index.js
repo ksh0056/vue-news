95eb6e79aca010c7d7202a7534146731de8040a3
Kim SeungHyuk <ksh9284@naver.com>
2020-03-29 15:28:37 +0900

    async & await

--- src/api/index.js
+++ src/api/index.js
@@ -6,16 +6,29 @@ const config = {
 };
 
 // 2. API 함수들을 정리
-function fetchList(pageName) {
-    return axios.get(`${config.baseUrl}${pageName}/1.json`)
+async function fetchList(pageName) {
+    try {
+        return axios.get(`${config.baseUrl}${pageName}/1.json`)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 function fetchUserInfo(username) {
-    return axios.get(`${config.baseUrl}user/${username}.json`)
+    try {
+        return axios.get(`${config.baseUrl}user/${username}.json`)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 function fetchCommentItem(id) {
-    return axios.get(`${config.baseUrl}item/${id}.json`)
+    try {
+        return axios.get(`${config.baseUrl}item/${id}.json`)
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 export {
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,30 +1,47 @@
 import { fetchList, fetchUserInfo, fetchCommentItem } from '../api/index.js';
 
 export default {
-    FETCH_USER({ commit }, name) {
-        return fetchUserInfo(name)
-            .then(({ data }) => {
-                commit('SET_USER', data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
+    //promise
+    // FETCH_USER({ commit }, name) {
+    //     return fetchUserInfo(name)
+    //         .then(({ data }) => {
+    //             commit('SET_USER', data)
+    //         })
+    //         .catch(error => {
+    //             console.log(error)
+    //         })
+    // },
+    // FETCH_ITEM({ commit }, id) {
+    //     return fetchCommentItem(id)
+    //         .then(({ data }) => {
+    //             commit('SET_ITEM', data)
+    //         })
+    //         .catch(error => {
+    //             console.log(error)
+    //         })
+    // },
+    // FETCH_LIST({ commit }, pageName) {
+    //     return fetchList(pageName)
+    //         .then(response => {
+    //             commit('SET_LIST', response.data);
+    //             return response;
+    //         })
+    //         .catch(error => console.log(error))
+    // },
+    // async
+    async FETCH_USER({ commit }, name) {
+        const response = await fetchUserInfo(name)
+        commit('SET_USER', response.data)
+        return response
     },
-    FETCH_ITEM({ commit }, id) {
-        return fetchCommentItem(id)
-            .then(({ data }) => {
-                commit('SET_ITEM', data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
+    async FETCH_ITEM({ commit }, id) {
+        const response = await fetchCommentItem(id)
+        commit.commit('SET_ITEM', response.data)
+        return response
     },
-    FETCH_LIST({ commit }, pageName) {
-        return fetchList(pageName)
-            .then(response => {
-                commit('SET_LIST', response.data);
-                return response;
-            })
-            .catch(error => console.log(error))
+    async FETCH_LIST({ commit }, pageName) {
+        const response = await fetchList(pageName)
+        commit('SET_LIST', response.data)
+        return response
     }
 }
